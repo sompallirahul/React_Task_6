e38f2c102b2f8b445316297c555f0171f3da2559
sompallirahul <139631225+sompallirahul@users.noreply.github.com>
2023-10-17 22:21:05 +0530

    Add files via upload

---
+++ src/Portfolio.js
@@ -0,0 +1,37 @@
+// Portfolio.js
+import React from 'react';
+import './Portfolio.css';
+
+const Portfolio = () => {
+  return (
+    <div className="portfolio">
+      <header>
+        <h1>Sompalli Rahul</h1>
+        <p>Web Developer</p>
+      </header>
+      <section className="about">
+        <h2>About Me</h2>
+        <p>
+          Welcome to my portfolio! I am a passionate web developer with a strong focus on creating responsive and user-friendly web applications.
+        </p>
+      </section>
+      <section className="projects">
+        <h2>Projects</h2>
+        <div className="project">
+          <h3>Project 1</h3>
+          <p>Developed a code that gives registered state of a phone number.</p>
+        </div>
+        <div className="project">
+          <h3>Project 2</h3>
+          <p>Developed rock-paper-scissors game which you can play with computer.</p>
+        </div>
+      </section>
+      <section className="contact">
+        <h2>Contact</h2>
+        <p>You can reach me at levakurakeshreddy@gmail.com</p>
+      </section>
+    </div>
+  );
+};
+
+export default Portfolio;
